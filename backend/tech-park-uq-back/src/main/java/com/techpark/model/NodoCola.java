c0b39343592833916644ec96db5f8025cd69f493
Juan. Maldonado <juanmaldo0618@gmail.com>
2026-05-07 20:21:20 -0500

    feat(model): agregar clase NodoCola para manejo de prioridades

---
+++ backend/tech-park-uq-back/src/main/java/com/techpark/model/NodoCola.java
@@ -0,0 +1,13 @@
+package main.java.com.techpark.model;
+
+public class NodoCola<T> {
+    public T dato;
+    public int prioridad;
+    public NodoCola<T> siguiente;
+
+    public NodoCola(T dato, int prioridad) {
+        this.dato = dato;
+        this.prioridad = prioridad;
+        this.siguiente = null;
+    }
+}
