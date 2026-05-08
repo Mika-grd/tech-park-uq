f1680c30bdb470e77fdfa449d221bebe7c30f6d3
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2026-05-08 10:32:19 -0500

    feat(applicartion): Add the application and the resources for setting up the H2 database

---
+++ backend/tech-park-uq-back/src/main/java/com/techpark/TechParkApplication.java
@@ -0,0 +1,11 @@
+package com.techpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechParkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechParkApplication.class, args);
+    }
+}
