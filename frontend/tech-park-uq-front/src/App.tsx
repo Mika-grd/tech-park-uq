1d37cd0ef06f0bd10db44deece7f17cdad4146ec
Mika-grd <miguela.betancourtc@uqvirtual.edu.co>
2026-05-08 22:49:27 -0500

    feat: inicializar estructura del front con vite y tailwind

---
+++ frontend/tech-park-uq-front/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main className="min-h-screen bg-zinc-950 text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">
+          Tech Park UQ
+        </h1>
+
+        <p className="text-zinc-400 mt-4">
+          Frontend React + Vite + Tailwind funcionando
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default App
